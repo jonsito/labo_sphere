6c42f75e71d3fdc9baf12c64eb06b65f829e0f43
jantonio <jonsito@gmail.com>
2020-06-08 14:05:30 +0200

    trying to provide user name in console access

--- web/SSHy_wrapper.php
+++ web/SSHy_wrapper.php
@@ -4,17 +4,18 @@ $host=http_request("host","s","");
 $user=http_request("user","s","");
 if($host==="") { readfile(__DIR__."/../denied.html"); exit(0); }
 $server=gethostname();
-$url="'wss://{$server}:6001/{$user}@{$host}:22'";
+$url="wss://{$server}.lab.dit.upm.es:6001/{$host}.lab.dit.upm.es:22";
 ?>
 <html lang="es">
 <head>
     <title>Lab-DIT SSH Web access to host: <?php echo "$host"?> </title>
     <meta charset="utf-8">
 	<script type="text/javascript">
-		var wsproxyURL = <?php echo $url; ?>
-	</script>
+        var wsproxyURL = '<?php echo $url; ?>';
+        var wsUserName = '<?php echo $user; ?>';
+    </script>
 	<link rel="stylesheet" href="/labo_sphere/web/css/xterm.css" async/>
-	<script type="text/javascript" src="/labo_sphere/web/libs/combinedJS.comb.js" async></script>
+	<script type="text/javascript" src="/labo_sphere/web/libs/combinedJS.comb_2.js" async></script>
 </head>
 <body>
 	<div id="terminal"></div>
