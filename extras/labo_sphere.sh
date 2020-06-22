80931b5251bd0b8913ed4e9c5c2bcf17f362337c
jantonio <jonsito@gmail.com>
2020-06-22 14:01:43 +0200

    labo_sphere.sh find_first host. first try

--- extras/labo_sphere.sh
+++ extras/labo_sphere.sh
@@ -5,12 +5,34 @@
 # centralizar las tareas en maestro, de manera que haya una única máquina que controle
 # adicionalmente esto evita problemas de seguriddd, pues en maestro podemos restringir
 # a este script los comandos que se pueden ejecutar desde acceso.lab via ssh
-REPORT=/var/log/labo_sphere.log
 
+BASE=/data/maestro3-labadmin/servicios_ubuntu-18.04
+REPORT=/var/log/labo_sphere.log
+STATUSFILE=${BASE}/estado_clientes.log
+
+source ${BASE}/lista_maquinas
 # buscar un equipo apagado de la zona deseada y encenderlo
 # parametro: zona
 find_freehost() {
-  echo ""
+  lista=""
+  case $1 in
+    "laboA" ) lista="${A127}" ;;
+    "laboB" ) lista="${B123}" ;;
+    "virtual" ) lista="${REMOTO}" ;;
+    "macs" ) lista="${MACS}" ;;
+    * ) echo ""; return ;;
+  esac
+  #cogemos lista de equipos del grupo escogido
+  cp /dev/null /tmp/find_freehost.$$
+  for i in $lista; do
+    grep "Client:$i" ${STATUS_FILE} >> /tmp/find_freehost.$$
+  done
+  # ordenamos segun numero encendido + ocupacion y cogemos el primero
+  equipo=`sed -e 's/Client://g' -e 's/ State:/ /g' -e 's/ Server:[b-].* Users:/ /g' /tmp/find_freehost.$$ | sort -k 2 | head -1`
+  rm -f /tmp/find_freehost.$$
+  # retornamos equipo seleccionado
+  echo $equipo
+  return
 }
 
 # arranca un servidor vnc en
@@ -44,15 +66,19 @@ case $1 in
       # wake up selected host
       bgjob /usr/local/bin/wakeup.sh -q $2
       # return #return wss://acceso.lab.dit.upm.es:6001/host:22
+      sleep 10
+      echo "wss://acceso.lab.dit.upm.es:6001/${host}:22"
       ;;
   "vnc_console" ) # user zone
       # locate free host
       host=$(find_freehost $2)
       # wake up selected host
       bgjob /usr/local/bin/wakeup.sh -q $2
+      sleep 10
       # create vnc server with session for user@host ( passwd='conectar' )
       port=ssh $host "echo $user conectar "
       # return wss://acceso.lab.dit.upm.es:wsport/host:vncport
+      echo "wss://acceso.lab.dit.upm.es:6001/${host}:${port}"
       ;;
   "tunnel" ) # zone
       # locate free host
@@ -63,6 +89,7 @@ case $1 in
       # return command to execute
   ;;
   "poll" )
+    /usr/local/bin/informemaq.sh -q remoto laboratorios macs
   ;;
   "help" )
       bgjob echo "hola mundo"
