c2a0ef66b0cf324479770c794b96347ab6b49722
jantonio <jonsito@gmail.com>
2020-06-25 11:32:33 +0200

    add an extra field to allow direct selection of host

--- web/libs/formatters.js
+++ web/libs/formatters.js
@@ -7,3 +7,12 @@ function statusStyler(val,row,idx) {
     else if (val==="Error") color="#FF8020";
     return 'background-color:'+color;
 }
+
+$.extend($.fn.validatebox.defaults.rules, {
+    regexp: {
+        validator: function(value, param){
+            return new RegExp(param[0]).test(value);
+        },
+        message: 'Please enter valid host name. i.e: l056'
+    }
+});
