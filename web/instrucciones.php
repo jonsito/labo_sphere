cb96b16965892fce0f4c2ee200d4f2ebc23267e7
jantonio <jonsito@gmail.com>
2020-07-09 13:01:54 +0200

    more and more work on main page (3)

--- web/instrucciones.php
+++ web/instrucciones.php
@@ -15,7 +15,7 @@ $disabled=($host==="-")?'disabled="disabled"':'';
     <dt><strong>Conexiones mediante interfaz web</strong><br/>&nbsp;<br/></dt>
     <dd>
         Una vez realizada la conexi&oacute;n puede acceder directamente al equipo seleccionado
-        mediante el uso del navegador ( Firefox, Chrome, Safari o Edge )<br/>&nbsp;<br/>
+        usando el navegador ( Firefox, Chrome, Safari o Edge )<br/>&nbsp;<br/>
         <table style="width:100%">
             <tr>
                 <td style="width:180px">
@@ -39,9 +39,10 @@ $disabled=($host==="-")?'disabled="disabled"':'';
                 </td>
             </tr>
         </table>
-        <br/>&nbsp<br/>
+        <br/>La acci&oacute;n seleccionada abrir&aacute; una nueva ventana, por lo que deberá habilitar el permitir
+        desplegar ventanas emergentes para esta p&aacute;gina<br/>&nbsp;<br/>
     </dd>
-    <dt><strong>Acceso mediante conexi&oacute;n segura (SSH) </strong><br/>&nbsp;<br/></dt>
+    <dt><strong>Acceso mediante conexi&oacute;n segura en modo texto (SSH) </strong><br/>&nbsp;<br/></dt>
     <dd>
         Para acceder en modo texto al ordenador seleccionado, una vez iniciada la sesi&oacute;n
         basta con conectarse al ordenador elegido utilizando un cliente de SSH. Por ejemplo desde Linux/Mac:
@@ -55,13 +56,32 @@ $disabled=($host==="-")?'disabled="disabled"':'';
     </dd>
     <dt><strong>Acceso mediante escritorio remoto (VNC)</strong><br/>&nbsp;<br/></dt>
     <dd>
-        ( pending )
+        Si no desea utilizar el escritorio remoto web y desea utilizar uno propio (p.e: Remina, TightVnc, RealVNC, etc ),
+        &eacute;ste deberá ser instalado en su equipo local. El cliente de acceso Remoto que viene de serie con Windows no
+        es válido, pues utliza un protocolo distinto ( RDP en lugar de RFB )
+        <br/>&nbsp;<br/>
+        Para conectarse al laboratorio, una vez abierta la sesión, deberá utilizar como dirección de acceso,
+        la indicada en la ventana de conexión. (p.e: l133.lab.dit.upm.es ) y el puerto 5900/TCP.
+        <br/>
+        Es recomendable que el cliente soporte encriptación SSL
+        <br/>&nbsp;<br/>
+        Una vez abierto el cliente VNC aparecerá la pantalla de login/contraseña del equipo como si se estuviera frente
+        a la consola.
+        <br/>
+        Ejemplo desde Linux:
+        <pre>
+        vncviewer &lt;ordenador&gt;.lab.dit.upm.es:5900;
+        </pre>
+        Para acceder desde un Mac-OSX no es necesario instalar servidor VNC adicional: se puede usar el servidor que
+        el navegador Safari trae de serie, abriendo la dirección: <em>vnc://&lt;ordenador&gt;.lab.dit.upm.es:5900</em>
+        <br/>&nbsp;<br/>
     </dd>
-    <dt><strong>Acceso mediante cliente NX (X2Go)</strong><br/></dt>
+    <dt><strong>Acceso mediante cliente NX (X2Go)</strong><br/>&nbsp;<br/></dt>
     <dd>
         ( pending )
+        <br/>&nbsp;<br/>
     </dd>
-    <dt><strong>Cierre de la sesi&oacute;n</strong><br/></dt>
+    <dt><strong>Cierre de la sesi&oacute;n</strong><br/>&nbsp;<br/></dt>
     <dd>
         Para cerrar la sesión debe acceder a la pantalla de acceso remoto basta con pulsar "Cerrar Sesi&oacute;n"
         en la ventana de "Datos de conexi&oacute;n"<br/>
