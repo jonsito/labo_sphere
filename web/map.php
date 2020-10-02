af008f177a2db00a0de3c12b4bafd3896068d8df
jantonio <jonsito@gmail.com>
2020-10-02 12:26:03 +0200

    admin map: rework. Now use transparent png and change background-color
    Instead of reload from server each icon on status change, just use one
    base image and change background

--- web/map.php
+++ web/map.php
@@ -1,17 +1,14 @@
 <?php
-
 $mapfile=__DIR__."/../config/layout_labo.map";
 $rows=@file($mapfile,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
 if (!$rows) {
     echo ("<strong>Cannot find mapfile '{$mapfile}'</strong>");
     return;
 }
-echo '<table style="border:0">';
+echo "<p>Mapa de recursos del laboratorio</p>";
 foreach ($rows as $row) {
-    echo "<tr>";
     $items=explode(":",$row);
     foreach ($items as $item) {
-        echo '<td id="img_{$item}" style="padding:0px;background:#C0C000">';
         switch ($item) {
             case "wall":
             case "wind":
@@ -19,15 +16,16 @@ foreach ($rows as $row) {
             case "null":
             case "none":
             case "serv":
-                echo "<img src=\"/labo_sphere/web/images/icons/{$item}.png\" width=\"30\" height=\"30\" alt=\"{$item}\">\n";
+                echo "<img style=\"border:0\" src=\"/labo_sphere/web/images/icons/{$item}.png\"  alt=\"{$item}\">\n";
                 break;
             default:
-                echo "<img id=\"img_{$item}\" src=\"/labo_sphere/web/getImage.php?host={$item}\" width=\"30\" height=\"30\" alt=\"{$item}\">\n";
+                echo "<span  id=\"img_{$item}\"  style=\"background-color:#C0C000\">\n";
+                echo "<img style=\"border:0\" src=\"/labo_sphere/web/getImage.php?host={$item}\" alt=\"{$item}\">\n";
+                echo "</span>";
                 break;
         }
-        echo "</td>";
+        echo "<br/>";
     }
-    echo "</tr>";
 }
 echo "</table>"
 ?>
