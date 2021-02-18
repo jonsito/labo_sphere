dcff57d61802c62b28f83480804e1a08294ebc73
jantonio <jonsito@gmail.com>
2021-02-18 12:10:49 +0100

    map: add menus for sockets

--- web/map.php
+++ web/map.php
@@ -22,25 +22,22 @@ foreach ($rows as $row) {
                 echo "<img style=\"border:0\" src=\"/labo_sphere/web/images/icons/{$a[0]}.png\"  alt=\"{$item}\">";
                 break;
             case "sock":
-                ?><a href="#" class="cl_sock" id="sock_<?php echo $item; ?>" >
-                    <img style="border:0"
+                ?><a href="#" class="cl_sock" id="sock_<?php echo $item; ?>" ><img style="border:0"
                         src="/labo_sphere/web/images/icons/<?php echo $a[0]; ?>.png"
                         alt="<?php echo $item; ?>"
-                    class="easyui-tooltip" data-options="
-                        position: 'top',
-                        onShow: function(e) { $(this).tooltip('update',getSockTip('<?php echo $item; ?>')); }
-                    "
-                ></a><?php
+                        class="easyui-tooltip" data-options="
+                            position: 'top',
+                            onShow: function(e) { $(this).tooltip('update',getSockTip('<?php echo $item; ?>')); }
+                        "></a><?php
                 break;
             default:
                 ?><a href="#" class="cl_menu" id="img_<?php echo $a[0]; ?>" ><img style="border:0"
-                         src="/labo_sphere/web/getImage.php?host=<?php echo $a[0]; ?>"
-                         alt="<?php echo $item; ?>"
-                         class="easyui-tooltip" data-options="
-                            position: 'top',
-                            onShow: function(e) { $(this).tooltip('update',getToolTip('<?php echo $a[0]; ?>')); }
-                         "
-                    ></a><?php
+                     src="/labo_sphere/web/getImage.php?host=<?php echo $a[0]; ?>"
+                     alt="<?php echo $item; ?>"
+                     class="easyui-tooltip" data-options="
+                        position: 'top',
+                        onShow: function(e) { $(this).tooltip('update',getToolTip('<?php echo $a[0]; ?>')); }
+                     "></a><?php
                 break;
         }
     }
@@ -60,12 +57,29 @@ echo "</p>"
     <div data-options="iconCls:'icon-console'" onclick="fireActionFromMap('console')">SSH Console</div>
 </div>
 
+<div id="sock_menu" style="width:120px;">
+    <div><strong>PowerIP</strong>: <span id="current_sock"></span></strong></div>
+    <div class="menu-sep"></div>
+    <div data-options="iconCls:'icon-redo'" onclick="fireActionFromMap('poweron')">Power On</div>
+    <div data-options="iconCls:'icon-reload'" onclick="fireActionFromMap('restart')">Restart</div>
+    <div data-options="iconCls:'icon-undo'" onclick="fireActionFromMap('poweroff')">Power Off</div>
+</div>
+
 <script>
     $(function(){
         $('#map_menu').menu();
+        $('#sock_menu').menu();
         $(".cl_menu").bind('contextmenu',function(e){
             e.preventDefault();
-            $('#current_host').html(e.target.alt);
+            $('#current_host').html(e.target.alt.split(",")[0]);
+            $('#map_menu').menu('show', {
+                left: e.pageX,
+                top: e.pageY
+            });
+        });
+        $(".cl_sock").bind('contextmenu',function(e){
+            e.preventDefault();
+            $('#current_sock').html(e.target.alt.split(",")[1]);
             $('#map_menu').menu('show', {
                 left: e.pageX,
                 top: e.pageY
